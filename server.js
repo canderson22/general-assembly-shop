2483a7e3aadc6fd5b5ad1b51bf5b7b1a81f11b73
Charles Anderson <charlesandreanderson22@gmail.com>
2017-11-01 16:37:56 -0700

    initial backend setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const
+  dotenv = require('dotenv').load(),
+  express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  logger = require('morgan'),
+  mongoose = require('mongoose'),
+  MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/general-assembly'
+  PORT = process.env.PORT || 3001
+//   userRoutes = require('./routes/users')
+//
+
+mongoose.connect(MONGODB_URI, (err) => {
+    console.log(err || `Connected to MongoDB.`)
+});
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/api', (req, res) => {
+    res.json({message: 'API ROOT'})
+})
+
+app.use('/api/users', userRoutes)
+
+app.listen(PORT, (err) => {
+    console.log(err || `Listening on port 🔥 ${PORT}`)
+})
