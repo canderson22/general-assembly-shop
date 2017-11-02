e1e3997a4af6dbb33be45f6334ac000f765681af
Charles Anderson <charlesandreanderson22@gmail.com>
2017-11-02 12:21:49 -0700

    added user model and controller functions

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,43 @@
+const
+    mongoose = require('mongoose'),
+    User = require('../models/User')
+
 module.exports = {
     index: (req, res) => {
-        res.json({success: true, message: 'All users'})        
+         User.find({}, (err, users) => {
+             res.json({success: true, users})
+         })       
     },
 
     create: (req, res) => {
-        res.json({success: true, message: 'created new user'})
+        User.create(req.body, (err, user) =>{
+            if(err) return res.json({success: false, error})
+            res.json({success: true, user})
+        })
+    },
+
+    show: (req, res) => {
+        User.findById(req.params.id, (err, user) => {
+            if(err) return console.log(err)
+            res.json({user})
+        })
+    },
+
+    update: (req, res) => {
+        User.findById(req.params.id, (err, user) => {
+            if(err) return console.log(err)
+            Object.assign(user, req.body)
+            user.save((err, updatedUser) => {
+                if(err) return console.log(err)
+                res.json({updatedUser})
+            })
+        })
+    },
+
+    destroy: (req, res) => {
+        User.findByIdAndRemove(req.params.id, (err, user) => {
+            if(err) return console.log(err)
+            res.json({success: true, msg: `deleted user ${user}`})
+        })
     }
 }
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const
+  mongoose = require('mongoose'),
+  bcrypt = require('bcrypt-nodejs'),
+  userSchema = new mongoose.Schema({
+      f_name: { type: String, required: true },
+      l_name: { type: String, required: true },
+      email: { type: String, required: true, unique: true },
+      password: { type: String, required: true },
+      orders: { type: Array }
+  })
+//
+
+
+// added to the user model to hash passwords
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+}
+
+// added to user model to compare password syncs 
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+// middleware check if password was changed if so encrypt new password before
+//saving
+userSchema.pre('save', function(next) {
+  if(this.isModified('password')) {
+    this.password = this.generateHash(this.password)
+  }
+  next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
