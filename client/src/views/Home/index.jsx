a6a4b78c14a59c2d47197114ba639a3d04477e85
Charles Anderson <charlesandreanderson22@gmail.com>
2017-11-09 14:24:49 -0800

    home page

--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 
+import './home.css'
+
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className='HOME'>
+           <div className='parallax-container valign-wrapper'>
+                <div className='parallax'>
+                    <img src="/productImages/ga-background.jpg" alt="unsplashed background img"/>
+                </div>
+           </div> 
+           <div className="container">
+                <div className="section">
+                    <div className='center-align'>
+                        <img src="/productImages/ga-logo.jpeg" alt="ga logo"/>
+                        <h1 className='heading'>SHOP</h1>
+                        <div className='divider'></div>
+                        <h2>Your one way stop and shop for swagged out gear and supplies</h2>
+                    </div>     
+                </div>
+            </div>
+           <div className='parallax-container valign-wrapper'>
+                <div className='parallax'>
+                    <img src="/productImages/shirt-bg.jpg" alt="unsplashed background img"/>
+                </div>
+           </div> 
+           <div className="container">
+                <div className="section">
+                    <div className='center-align'>
+                        <h2>
+                            Don't be the one to miss out!
+                        </h2>
+                        <p className='flow-text'>
+                            Get all your gear quick and easy. Don't miss out on this year's end of year sale.
+                        </p>
+                    </div>     
+                </div>
+            </div>
+            <div className='parallax-container valign-wrapper'>
+                <div className='parallax'>
+                    <img src="https://ga-shop.s3.amazonaws.com/production/store/locationmasthead/11/image/retina-3af887010db0db7a24bcae3ab47100a7.jpg" alt="unsplashed background img"/>
+                </div>
+           </div> 
+        </div>
     )
 }
 
